32df69eb9a783dc5cf1ecd878e544e926edb3c54
Jonathan Muñoz Morales <jonathan_tuenty@hotmail.com>
2025-06-08 23:41:19 +0200

    feat: updated cors

--- src/main/java/com/detallesunicos/v1/security/SecurityConfig.java
+++ src/main/java/com/detallesunicos/v1/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://www.detallesunicos3d.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
